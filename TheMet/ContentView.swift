006f8dc198304234045d3a78242d8c331cb0edb1
Lily Ton <loi.nguyen1099@gmail.com>
2026-02-26 11:43:14 -0500

    Initial Commit

---
+++ TheMet/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TheMet
+//
+//  Created by Loi Ton on 2/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheMet/TheMetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMetApp.swift
+//  TheMet
+//
+//  Created by Loi Ton on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct TheMetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
